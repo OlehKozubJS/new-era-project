7d2af189b50eedf3487f4d2d36058d72bcc9889a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-09-18 21:57:06 +0300

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -3,3 +3,5 @@ import css from "./style.module.css";
 const RangeField = css.RangeField;
 const RangeDial = css.RangeDial;
 const RangeIndicator = css.RangeIndicator;
+
+export { RangeField, RangeDial, RangeIndicator };
