97bb507188301b9d0163b9ad312c7685a5a60b6c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-09-18 21:42:14 +0300

    add range markup and styles file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 const App = () => {
   return (
     <>
       <h1>Hello World!</h1>
       <h2>Progress Always Wins!</h2>
       <p>I am back.</p>
+
+      <div>
+        <div></div>
+      </div>
+      <p></p>
     </>
   );
 };
