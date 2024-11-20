76e43dc1c8b916efd5ddc589622c131f30965628
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-20 06:49:50 +0200

    Update SelectorDispatchComponent.jsx

--- src/ultimateRedux/SelectorDispatchComponent.jsx
+++ src/ultimateRedux/SelectorDispatchComponent.jsx
@@ -1,3 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 
 import { setCommonReducer, getCommonReducer } from "./slice";
+
+const SelectorDispatchComponent = () => {
+  return <div></div>;
+};
+
+export { SelectorDispatchComponent };
