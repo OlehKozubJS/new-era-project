78f85e1940eb1bded1d4fb9ead728c82f5d9d440
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-21 01:05:03 +0200

    Update slice.js

--- src/ultimateRedux/slice.js
+++ src/ultimateRedux/slice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const slice = createSlice({
   name: "commonReducer",
-  initialState: { state: {} },
+  initialState: { state: null },
   reducers: {
     setState(state, action) {
       return { state: action.payload };
