9b46fb6c9d97cbb628593afc8426a9b7ad727370
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-20 14:51:10 +0200

    Update index.js

--- src/ultimateRedux/index.js
+++ src/ultimateRedux/index.js
@@ -1,4 +1,10 @@
 import { ReducerComponent } from "./ReducerComponent";
 import { SelectorDispatchComponent } from "./SelectorDispatchComponent";
+import { setCommonReducer, getCommonReducer } from "./slice";
 
-export { ReducerComponent, SelectorDispatchComponent };
+export {
+  ReducerComponent,
+  SelectorDispatchComponent,
+  setCommonReducer,
+  getCommonReducer,
+};
