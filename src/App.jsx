6df1b5409d3500c5ad0ebba7734844939af1fd16
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-20 17:06:26 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { ReducerComponent } from "./ultimateRedux";
+import { setCommonReducer, getCommonReducer } from "./ultimateRedux";
 
 import { SimplestCustomRange } from "./SimplestCustomRange";
 
