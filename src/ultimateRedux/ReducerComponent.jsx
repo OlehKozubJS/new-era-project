f927ef10108770e1f8fc10f792de1811838742ff
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-20 06:27:33 +0200

    Update ReducerComponent.jsx

--- src/ultimateRedux/ReducerComponent.jsx
+++ src/ultimateRedux/ReducerComponent.jsx
@@ -2,8 +2,12 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { ReducerComponentBase } from "./ReducerComponentBase";
 
+import { setCommonReducer, getCommonReducer } from "./slice";
+
 const ReducerComponent = ({ children, reducers, blacklist, whitelist }) => {
   const dispatch = useDispatch();
+
+  const store = useSelector();
 
   return <ReducerComponentBase>{children}</ReducerComponentBase>;
 };
