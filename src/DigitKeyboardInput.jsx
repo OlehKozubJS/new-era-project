5e9131b44dadf105e48a6dc734ddddaea5451f71
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-21 23:55:15 +0200

    Update DigitKeyboardInput.jsx

--- src/DigitKeyboardInput.jsx
+++ src/DigitKeyboardInput.jsx
@@ -1,3 +1,16 @@
 import { useState, useEffect } from "react";
 
 import { characters } from "./dataBase";
+
+const DigitKeyboardInput = () => {
+  return (
+    <>
+      <p></p>
+      <button>1</button>
+      <button>2</button>
+      <></>
+    </>
+  );
+};
+
+export { DigitKeyboardInput };
