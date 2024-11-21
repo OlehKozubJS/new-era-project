0cb99782f1a3db27b9406cf85b86d079cb540676
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-22 01:34:35 +0200

    Update DigitKeyboardInput.jsx

--- src/DigitKeyboardInput.jsx
+++ src/DigitKeyboardInput.jsx
@@ -3,14 +3,20 @@ import { useState, useEffect } from "react";
 import { characters } from "./dataBase";
 
 const DigitKeyboardInput = () => {
+  const [firstDigit, setFirstDigit] = useState(0);
+  const [secondDigit, setSecondDigit] = useState(0);
+
+  const handleClick = (event) => {};
+
   return (
     <>
       <p></p>
-      {[0].map((item, index, array) => {
-        if (item < array.length - 1) {
-          array.push(item + 1);
-        }
-        return <button key={index}>{item}</button>;
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((digit) => {
+        return (
+          <button key={digit} value={digit}>
+            {digit}
+          </button>
+        );
       })}
     </>
   );
