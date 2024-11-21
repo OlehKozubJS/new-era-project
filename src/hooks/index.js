2d607c07ce38c3cd018e9a5dba5a32ce57b8cc14
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-21 20:44:38 +0200

    Create index.js

---
+++ src/hooks/index.js
@@ -0,0 +1,23 @@
+import { carModel, setCarModel, getCarModel } from "./carModelReducer";
+import {
+  selectedCars,
+  setSelectedCars,
+  getSelectedCars,
+} from "./selectedCarsReducer";
+import {
+  handleEventListener,
+  handleInterval,
+  handleTimeOut,
+} from "./useEffectHandlers";
+
+export {
+  carModel,
+  setCarModel,
+  getCarModel,
+  selectedCars,
+  setSelectedCars,
+  getSelectedCars,
+  handleEventListener,
+  handleInterval,
+  handleTimeOut,
+};
