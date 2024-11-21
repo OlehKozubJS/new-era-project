feced51951fa20e8aa555e1a82877468678ec907
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-21 23:10:54 +0200

    Update SimplestCustomRange.jsx

--- src/SimplestCustomRange.jsx
+++ src/SimplestCustomRange.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-
 import { useDispatch, useSelector } from "react-redux";
+
+import { handleEventListener } from "./hooks";
 
 import { setState, getState } from "./ultimateRedux";
 
