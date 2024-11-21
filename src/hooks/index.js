f835fbd187815d55f9b19e82ed0ac5c13f7ee313
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-21 23:14:20 +0200

    Update index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,23 +1,7 @@
-import { carModel, setCarModel, getCarModel } from "./carModelReducer";
-import {
-  selectedCars,
-  setSelectedCars,
-  getSelectedCars,
-} from "./selectedCarsReducer";
 import {
   handleEventListener,
   handleInterval,
   handleTimeOut,
 } from "./useEffectHandlers";
 
-export {
-  carModel,
-  setCarModel,
-  getCarModel,
-  selectedCars,
-  setSelectedCars,
-  getSelectedCars,
-  handleEventListener,
-  handleInterval,
-  handleTimeOut,
-};
+export { handleEventListener, handleInterval, handleTimeOut };
