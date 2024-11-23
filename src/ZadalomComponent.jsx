e11b9a1e6482f306442cff9b91f794f431fd1a78
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-23 23:48:04 +0200

    Update ZadalomComponent.jsx

--- src/ZadalomComponent.jsx
+++ src/ZadalomComponent.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 
 const ZadalomComponent = () => {
+  const handleSubmit = (event) => {};
+
+  const handleChange = (event) => {};
+
   return (
     <div>
-      <form>
-        <textarea />
+      <form onSubmit={handleSubmit}>
+        <textarea onChange={handleChange} />
         <button type="submit"></button>
       </form>
       <p></p>
