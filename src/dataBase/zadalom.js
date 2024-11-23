1d24bb5f4b87704ea5d4541123517ceea055aa80
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-24 00:48:02 +0200

    Update zadalom.js

--- src/dataBase/zadalom.js
+++ src/dataBase/zadalom.js
@@ -63,4 +63,33 @@ const zadalomCyrillicLetters = {
   я: "ya",
 };
 
+const upperCaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
 export { zadalomLetters, zadalomCyrillicLetters };
