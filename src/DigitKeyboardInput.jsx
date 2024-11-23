3fe266ba51eb76b1343c75343c7d168a24308ee9
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-23 22:50:56 +0200

    Update DigitKeyboardInput.jsx

--- src/DigitKeyboardInput.jsx
+++ src/DigitKeyboardInput.jsx
@@ -10,7 +10,7 @@ const DigitKeyboardInput = () => {
   const [secondDigit, setSecondDigit] = useState(0);
 
   const handleDigit = (event) => {
-    const newDigit = Number(event.target.value);
+    const newDigit = Number(event.target?.value ?? event);
 
     if (isFirstDigit) {
       setFirstDigit(newDigit);
